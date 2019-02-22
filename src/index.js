5574ecda70dcbb2af8e3fbaca76cdb06ae0fe02b
oldblacktree <OldBlackTree@gmail.com>
2019-02-22 12:40:45 +0300

    This is the first decision that occurred to me

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let triangles = 0;
+  preferences.forEach(function (item, i, arr) {
+
+    let SpichoneeB = arr[item - 1];
+    let SpichoneeC = arr[SpichoneeB - 1];
+    if (i === SpichoneeC - 1) {
+      triangles++;
+    };
+    if (item == i + 1) {
+      triangles--;
+    };
+  });
+  return Math.round(triangles / 3);
+}
