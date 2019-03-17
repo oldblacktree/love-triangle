8c09181815f6ff9062fa323b0d210570774ca5c0
Danil Lukonin <OldBlackTree@gmail.com>
2019-03-17 10:52:19 +0300

    refactoring: add arrow function, delete statment Math.round

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
   let triangles = 0;
-  preferences.forEach(function (item, i, arr) {
+
+  preferences.forEach((item, i, arr) => {
 
     let SpichoneeB = arr[item - 1];
     let SpichoneeC = arr[SpichoneeB - 1];
+    
     if (i === SpichoneeC - 1) {
       triangles++;
     };
+    
     if (item == i + 1) {
       triangles--;
     };
   });
-  return Math.round(triangles / 3);
+  
+  return triangles / 3;
 }
